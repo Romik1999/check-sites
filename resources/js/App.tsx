b351c341dde2900f042225b8ca14b228a8a11bb2
roman <usankin.roma@gmail.com>
2023-11-05 04:11:01 +0400

    work on front

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -4,17 +4,26 @@ import Home from './pages/home';
 import Settings from "./pages/settings";
 import Users from "./pages/users";
 import Logs from "./pages/logs";
+import Layout from "./components/layout";
+import theme from "./theme";
+import {ThemeProvider} from "@mui/material/styles";
+import Login from "./pages/login";
 
 function App() {
     return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/settings" element={<Settings/>}/>
-                <Route path="/logs" element={<Logs/>}/>
-                <Route path="/users" element={<Users/>}/>
-            </Routes>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Layout>
+                <div className="App">
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/settings" element={<Settings/>}/>
+                        <Route path="/logs" element={<Logs/>}/>
+                        <Route path="/users" element={<Users/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                    </Routes>
+                </div>
+            </Layout>
+        </ThemeProvider>
     );
 }
 
